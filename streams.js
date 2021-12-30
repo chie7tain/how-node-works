cec43123550924acf6928948dd97e81478e0d0f1
chei7tain <fredrickokwuobi@gmail.com>
2021-12-30 18:52:10 +0100

    we used the fs.createReadStream to read a file chunk by chunk and for each chunk we write the chunk to the client however this can cause backpressure as the read

--- streams.js
+++ streams.js
@@ -3,9 +3,22 @@ const server = require("http").createServer();
 // we want to read the file
 server.on("request", (req, res) => {
   // first solution
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  // fs.readFile("test-file.txt", (err, data) => {
+  //   if (err) console.log(err);
+  //   res.end(data);
+  // });
+  // second solution
+  const readable = fs.createReadStream("test0-file.txt");
+  readable.on("data", (chunk) => {
+    res.write(chunk);
+  });
+  res.on("end", () => {
+    res.end("end of file");
+  });
+  readable.on("error", (err) => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end("error file not found");
   });
 });
 
